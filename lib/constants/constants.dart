16fec9487c5dd0c439a7fe7d6a48c41b698ea2fc
srhdp <srhdp@MacBook-Pro-2.local>
2025-05-06 15:05:15 +0700

    phone verify page

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -22,3 +22,10 @@ double width = 375.w;
 
 const String appBaseUrl = "http://localhost:6013";
 
+final List<String> verificationReasons = [
+  'Real-time Updates: Get instant notifications about your order status.',
+  'Direct Communication: A verified number ensures seamless communication.',
+  'Enhanced Security: Protect your account and confirm orders securely.',
+  'Effortless Rescheduling: Easily address issues with a quick call.',
+  'Exclusive Offers: Stay in the loop for special deals and promotions.'
+];
