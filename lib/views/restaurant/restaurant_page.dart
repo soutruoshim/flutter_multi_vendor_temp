7eede3bc3aa6ac0085f3c39bbb34681c52c523c1
srhdp <srhdp@MacBook-Pro-2.local>
2025-05-07 11:39:35 +0700

    resturant widget

--- lib/views/restaurant/restaurant_page.dart
+++ lib/views/restaurant/restaurant_page.dart
@@ -1,5 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:foodly/common/app_style.dart';
+import 'package:foodly/common/reusable_text.dart';
+import 'package:foodly/constants/constants.dart';
 import 'package:foodly/models/restaurants_model.dart';
+import 'package:foodly/views/restaurant/directions_page.dart';
+import 'package:foodly/views/restaurant/widget/restaurant_bottom_bar.dart';
+import 'package:get/get.dart';
 
 class RestaurantPage extends StatefulWidget {
   const RestaurantPage({super.key, required this.restaurant});
@@ -13,10 +22,64 @@ class RestaurantPage extends StatefulWidget {
 class _RestaurantPageState extends State<RestaurantPage> {
   @override
   Widget build(BuildContext context) {
-    print(widget.restaurant!.coords.address);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: kLightWhite,
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 230.h,
+                  width: width,
+                  child: CachedNetworkImage(
+                      fit: BoxFit.cover, imageUrl: widget.restaurant!.imageUrl),
+                ),
+                Positioned(
+                    bottom: 0, child: RestaurantBottomBar(widget: widget)),
+                Positioned(
+                    top: 40.h,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 12.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Get.back();
+                            },
+                            child: const Icon(
+                              Ionicons.chevron_back_circle,
+                              size: 28,
+                              color: kLightWhite,
+                            ),
+                          ),
+                          ReusableText(
+                              text: widget.restaurant!.title,
+                              style: appStyle(13, kDark, FontWeight.w600)),
+                          GestureDetector(
+                            onTap: () {
+                              Get.to(() => const DirectionsPage());
+                            },
+                            child: const Icon(
+                              Ionicons.location,
+                              size: 28,
+                              color: kLightWhite,
+                            ),
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
